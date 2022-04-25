434014379df3123972395f8a1c27d999fbb72c10
xiaoyun5520342 <1031882385@qq.com>
2022-04-25 15:42:35 +0800

    提交

---
+++ src/main/java/com/yhlo/oa/TestApplication.java
@@ -0,0 +1,17 @@
+package com.yhlo.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @create: 2022-04-25 15:30
+ * @description:
+ **/
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        // 启动spring-boot
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
