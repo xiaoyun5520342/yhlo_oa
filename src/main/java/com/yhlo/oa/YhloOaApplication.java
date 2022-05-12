6ca0b0b05a9b45e1e3af88cd3bff423dc75106c8
xiaoyun5520342 <1031882385@qq.com>
2022-05-12 14:42:41 +0800

    上下文对象管理

--- src/main/java/com/yhlo/oa/YhloOaApplication.java
+++ src/main/java/com/yhlo/oa/YhloOaApplication.java
@@ -6,20 +6,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
 @MapperScan(basePackages = {"com.yhlo.oa.mapper"})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = {"com.yhlo.oa"})
 public class YhloOaApplication extends Application {
 
+    protected static ConfigurableApplicationContext applicationContext;
+
     public static void main(String[] args) {
         // 先加载spring
-        SpringApplication.run(YhloOaApplication.class, args);
-        // 再启动主页面
-        launch(args);
+        CompletableFuture.supplyAsync(() -> applicationContext = SpringApplication.run(YhloOaApplication.class, args)).whenComplete((ctx, throwable) -> {
+            if (throwable != null) {
+                log.error("Failed to load spring application context: ", throwable);
+            } else {
+                // 再启动主页面
+                launch(args);
+            }
+        });
     }
 
     @Override
@@ -33,5 +45,13 @@ public class YhloOaApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(CommonUtil.getLogo());
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        if (applicationContext != null) {
+            applicationContext.close();
+        }
     }
 }
